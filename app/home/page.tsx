c87656dc6f2139f9a1695799475e8e3851a107ce
zhangyajun <1655623164@qq.com>
2024-05-07 18:01:42 +0800

    feat: 增加 Layout 组件

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -5,6 +5,10 @@ import Button from '@zx/backstage-components/dist/Button.min';
 import '@zx/backstage-components/dist/Button.min.css'
 import styles from "./home.module.scss";
 
+import Layout from '@zx/backstage-components/dist/Layout.min';
+import '@zx/backstage-components/dist/Layout.min.css'
+
+
 export const metadata = {
   title: 'Breadcrumb',
   description: 'Breadcrumb Home',
@@ -15,6 +19,11 @@ function Home(props) {
   const { name, count } = props;
   return (
     <>
+    <Layout title="测试" style={{height: 1000}}>
+      <Layout.Header>Header</Layout.Header>
+      <Layout.Content>Content</Layout.Content>
+      <Layout.Footer>Footer</Layout.Footer>
+    </Layout>
     <span>{name}</span>
     <span>{count}</span>
     <Button type="primary">基础使用</Button>
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,16 @@ import './global.css'
 import type { Metadata } from 'next'
 import { GeistSans } from 'geist/font/sans'
 import { GeistMono } from 'geist/font/mono'
-import { Navbar } from './components/nav'
 import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/next'
 import Footer from './components/footer'
 import { baseUrl } from './sitemap'
 import StoreProvider from './StoreProvider';
+import Layout from '@zx/backstage-components/dist/Layout.min';
+import '@zx/backstage-components/dist/Layout.min.css';
+import { Navbar } from './components/nav'
+import Header from './components/Header'
+import styles from './index.module.scss';
 
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
@@ -54,52 +58,29 @@ export default function RootLayout({
           GeistMono.variable
         )}
       >
-        <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
-          <header>
-            <h1>My wonderful blog</h1>
-            <p>My tagline</p>
-          </header>
-          <aside>
-            <nav>
-              <h2>My blogro11</h2>
-              <ul>
-                <li><a href='https://blog.example.com/'>Example Blog</a></li>
-              </ul>
-            </nav>
-            <nav>
-              <h2>Archives</h2>
-              <ol>
-                <li><a href='/last-post'>My last post</a></li>
-                <li><a href='/first-post'>My first post</a></li>
-              </ol>
-            </nav>
-          </aside>
-          <aside>
-            <h1>Twitter Feed</h1>
-            <blockquote cite="https://twitter.example.net/t31351234">
-              I am on vacation, writing my blog.
-            </blockquote>
-            <blockquote cite="https://twitter.example.net/t21219752">
-              I am going to go on vacation soon.
-            </blockquote>
-          </aside>
-          <article>
-            <h2>My last post</h2>
-            <p>This is my last post.</p>
-            <footer>
-              <p>
-                <a href="/last-post" rel="bookmark">Permalink</a>
-              </p>
-            </footer>
-          </article>
+        <body className="antialiased mx-4 mt-8 lg:mx-auto">
+          <div className={styles.layout}>
+          <Layout>
+            <Layout.Header>
+              <Header/>
+            </Layout.Header>
+            <Layout sider>
+              <Layout.Sider>
+                <Navbar/>
+              </Layout.Sider>
+              <Layout.Content>
+              {children}
+            </Layout.Content>
+            </Layout>
+            <Layout.Footer>
+              Footer
+            </Layout.Footer>
+          </Layout>
+          </div>
           <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
-            <Navbar />
-            {children}
-            <Footer />
             <Analytics />
             <SpeedInsights />
           </main>
-          <footer>This is footer</footer>
         </body>
       </html>
     </StoreProvider>
